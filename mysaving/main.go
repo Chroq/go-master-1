f183514e8b54ec3d8a1c1006c2829e598abbb0be
ChristopheLecroq <christophe.lecroq@gmail.com>
2024-03-27 13:54:16 +0100

    feat(saving): use interface to isolate calculate functions

--- mysaving/main.go
+++ mysaving/main.go
@@ -6,13 +6,80 @@ import (
 	"math/rand"
 )
 
+type SavingInterface interface {
+	CalculateSaving() float64
+	Name() string
+}
+
+type Simulation struct {
+	Salary float64
+	Year   int
+}
+
 type SavingMethod struct {
-	Name   string
-	method func(salary float64, year int) float64
+	name       string
+	simulation Simulation
+}
+
+func NewSavingMethod(name string, simulation Simulation) SavingMethod {
+	return SavingMethod{
+		name:       name,
+		simulation: simulation,
+	}
+}
+
+func (m SavingMethod) Name() string {
+	return m.name
+}
+
+type ClassicalSavingMethod struct {
+	SavingMethod
+	Rate float64
+}
+
+type ProgressiveSavingMethod struct {
+	SavingMethod
+	Rate float64
+}
+
+type PokerSavingMethod struct {
+	SavingMethod
+	MinRate float64
+	MaxRate float64
+}
+
+func (m ClassicalSavingMethod) CalculateSaving() float64 {
+	sum := 0.0
+	for i := 0; i < m.simulation.Year*12; i++ {
+		sum += m.simulation.Salary * m.Rate
+	}
+
+	return sum
+}
+
+func (m PokerSavingMethod) CalculateSaving() float64 {
+	sum := 0.0
+	for i := 0; i < m.simulation.Year*12; i++ {
+		sum += math.RoundToEven(m.MinRate + rand.Float64()*(m.MaxRate-m.MinRate)*m.simulation.Salary)
+	}
+
+	return sum
+}
+
+func (m *ProgressiveSavingMethod) CalculateSaving() float64 {
+	sum := 0.0
+	for i := 0; i < m.simulation.Year*12; i++ {
+		m.Rate += ProgressiveIncrease
+
+		sum += math.RoundToEven(m.simulation.Salary * m.Rate)
+	}
+
+	return sum
 }
 
 const (
-	Salary      float64 = 4000
+	Salary float64 = 4000
+
 	ClassicRate float64 = 0.15
 
 	ProgressiveRate     float64 = 0.05
@@ -23,48 +90,32 @@ const (
 )
 
 func main() {
-	year := 5
+	simulation := Simulation{
+		Salary: Salary,
+		Year:   5,
+	}
 
-	savingMethods := []SavingMethod{
-		{
-			Name: "Classic",
-			method: func(salary float64, year int) float64 {
-				return math.RoundToEven(salary * float64(year*12) * ClassicRate)
-			},
+	savingMethods := []SavingInterface{
+		ClassicalSavingMethod{
+			SavingMethod: NewSavingMethod("Classic", simulation),
+			Rate:         ClassicRate,
 		},
-		{
-			Name: "Poker",
-			method: func(salary float64, year int) float64 {
-				sum := 0.0
-				for i := 0; i < year*12; i++ {
-					rate := PokerMinRate + rand.Float64()*(PokerMaxRate-PokerMinRate)
-					sum += salary * rate
-				}
-
-				return math.RoundToEven(sum)
-			},
+		PokerSavingMethod{
+			SavingMethod: NewSavingMethod("Poker", simulation),
+			MinRate:      PokerMinRate,
+			MaxRate:      PokerMaxRate,
 		},
-		{
-			Name: "Progressive",
-			method: func(salary float64, year int) float64 {
-				rate := ProgressiveRate
-				sum := 0.0
-				for i := 1; i < year*12; i++ {
-					rate += ProgressiveIncrease
-					sum += salary * rate
-				}
-
-				return math.RoundToEven(sum)
-			},
+		&ProgressiveSavingMethod{
+			SavingMethod: NewSavingMethod("Progressive", simulation),
+			Rate:         ProgressiveRate,
 		},
 	}
 
-	fmt.Println("Salary: ", Salary)
-	fmt.Println("Year: ", year)
+	fmt.Println("Salary: ", simulation.Salary)
+	fmt.Println("Year: ", simulation.Year)
 	fmt.Println("Result : ")
 
 	for i := range savingMethods {
-		fmt.Printf("- %s: %.2f€ \n", savingMethods[i].Name, savingMethods[i].method(Salary, year))
+		fmt.Printf("- %s: %.2f€ \n", savingMethods[i].Name(), savingMethods[i].CalculateSaving())
 	}
-
 }
